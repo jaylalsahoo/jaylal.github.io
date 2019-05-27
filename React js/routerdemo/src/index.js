2a8e6951374710a6a6ae0af1f63be87968c97c5f
jaylalsahoo <jaylalsahoo@gmail.com>
2019-05-26 21:25:02 -0700

    company page has updated

--- React js/routerdemo/src/index.js
+++ React js/routerdemo/src/index.js
@@ -6,9 +6,9 @@ class App extends React.Component{
     render(){
         return(
             <Router>
-            <div className="container">
+            <div className="container-fluid">
               <div className="row">
-                <div className="col-md-5 offset-md-3">
+                <div className="col-md-8 offset-md-2">
                 <ul className="nav justify-content-center">
                     <li className="nav-item">
                        <Link className="nav-link active" to="/">Home</Link>
@@ -35,10 +35,37 @@ class App extends React.Component{
 
 class Home extends React.Component{
     render(){
+        
         return (
-            <div>
-                <p>Hello , This is the Home page content</p>
-            </div>
+            <div className="container-fluid">
+                <div className="row">
+                   <div className="col-md-12 offset-md-1">
+                   <h1>AwesomeCompany.Com</h1>
+                   </div>
+                 </div>
+                 <div className="row">
+                 <div className="pt-2 col-md-3">
+                    <h3>Feature #1</h3>
+                    <hr></hr>
+                    <p>some text here</p>
+                </div>
+                <div className="pt-2 col-md-3">
+                    <h3>Feature #2</h3>
+                    <hr></hr>
+                    <p>some text here</p>
+                </div>
+                <div className="pt-2  col-md-3">
+                        <h3>Feature #3</h3>
+                        <hr></hr>
+                        <p>some text here</p>
+                </div>
+                <div className="pt-2 col-md-3">
+                        <h3>Feature #4</h3>
+                        <hr></hr>
+                        <p>some text here</p>
+                </div>
+                 </div>
+             </div>
         );
     }
 
@@ -47,9 +74,48 @@ class Home extends React.Component{
 class Aboutus extends React.Component{
     render(){
         return (
-            <div>
-                <p>This is the Company Details Page</p>
-            </div>
+            <div className="container">
+                <div class="row">
+         <div className="col-md-12">
+        <h1 className="text-center">About us</h1>
+        <hr></hr>
+      </div>
+      </div>
+      <div className="row">
+        
+          <div className="col-md-8">
+            <h3>CEO</h3>
+            <p>some text about CEO here</p>
+          </div>
+          <div className="col-md-4">
+              <img src="./images/bill.png" height="100px" width="100px" />
+          </div>
+          
+      </div>
+      <div className="pt-2 row">
+        
+          <div className="col-md-8">
+            <h3>CTO</h3>
+            <p>some text about CEO here</p>
+          </div>
+          <div className="col-md-4">
+              <img src="./images/cto.png" height="100px" width="100px" />
+          </div>
+          
+      </div>
+      <div className="pt-2 row">
+        
+          <div className="col-md-8">
+            <h3>COO</h3>
+            <p>some text about CEO here</p>
+          </div>
+          <div className="col-md-4">
+              <img src="./images/coo.png" height="100px" width="100px" />
+          </div>
+          
+      </div>
+      </div>
+      
         );
     }
 
@@ -58,8 +124,14 @@ class Aboutus extends React.Component{
 class Contactus extends React.Component{
     render(){
         return (
-            <div>
-                <p>This is the Contact us Page</p>
+            <div className="container">
+                <div className="row">
+            <div className="col-md-12">
+                <h1 className=" text-center">Contact us</h1>
+                <hr></hr>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15551.929419156559!2d77.62950026952207!3d12.972980313785726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae16a418770391%3A0xb50f46b826501036!2sIndiranagar%2C+Bengaluru%2C+Karnataka!5e0!3m2!1sen!2sin!4v1551870768336" width="1200" height="600" frameborder="0" allowfullscreen></iframe>
+    </div>
             </div>
         );
     }
